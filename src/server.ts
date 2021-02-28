dfb08ee919e31f93e0c89e04f0dec474a9561400
Abilio Azevedo <abilio10@gmail.com>
2021-02-28 12:15:46 -0300

    feat: config port heroku

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import { app } from "./app";
-import "./docs";
+import { app } from './app';
+import './docs';
 
-app.listen(3333, () => console.log('🚀 Server is running on port 3333!'));
+app.listen(process.env.PORT || 3333, () =>
+  console.log('🚀 Server is running on port 3333!'),
+);
